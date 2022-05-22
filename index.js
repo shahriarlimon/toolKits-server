ad9d6c7d659a141e2c90923001f0b58f830c76d1
shahriarlimon <shahriarwebdev@gmail.com>
2022-05-23 03:30:51 +0600

    connected to mongodb

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.ypd6p.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+      await client.connect();
+      const toolCollection = client.db("tool_kits").collection("tools");
+      console.log('Mongodb connected');
+      
+    } finally {
+      
+    }
+  }
+  run().catch(console.dir);
+
 
 
 app.get("/", async (req, res) => {
