ff649dd486b06211ebe48315b1972390fcb459e7
shahriarlimon <shahriarwebdev@gmail.com>
2022-05-26 21:25:18 +0600

    finding cors error

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-app.use(cors());
+app.use(cors({
+  origin:true,
+  optionsSuccessStatus:200,
+  credentials:true
+}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.ypd6p.mongodb.net/?retryWrites=true&w=majority`;
